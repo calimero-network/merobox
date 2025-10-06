122203499b167172e60cf1ce3cf31b0657c4aed8
chefsale <sandi.fatic@gmail.com>
2025-10-06 16:58:32 +0200

    chore(example): echo first argument in echo-exported-value.sh

--- workflow-examples/scripts/echo-exported-value.sh
+++ workflow-examples/scripts/echo-exported-value.sh
@@ -2,8 +2,9 @@
 set -euo pipefail
 
 echo "[script] Running echo-exported-value.sh"
-echo "[script] This is a sample script executed inside the container."
-echo "[script] You can customize this script to perform validations or setup."
-echo "[script] Done."
 
-
+if [ $# -ge 1 ]; then
+  echo "Exported value is: $1"
+else
+  echo "Exported value is: <none>"
+fi
