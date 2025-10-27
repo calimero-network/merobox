72a4d9368e635012586297e0433c711ad3ae3deb
xilosada <xabi@calimero.network>
2025-10-27 04:14:13 +0100

    Bump version to 0.2.1 for release

--- merobox/__init__.py
+++ merobox/__init__.py
@@ -2,6 +2,6 @@
 Merobox - A Python CLI tool for managing Calimero nodes in Docker containers.
 """
 
-__version__ = "0.2.0"
+__version__ = "0.2.1"
 __author__ = "Merobox Team"
 __email__ = "team@merobox.com"
