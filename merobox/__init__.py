714fe8502364ed71b8cf3192bdb3bc7cb877e6cc
Ronit <147048280+rtb-12@users.noreply.github.com>
2025-11-08 14:35:13 +0530

    chore: bump version to 0.2.3 (#77)

--- merobox/__init__.py
+++ merobox/__init__.py
@@ -2,6 +2,6 @@
 Merobox - A Python CLI tool for managing Calimero nodes in Docker containers.
 """
 
-__version__ = "0.2.2"
+__version__ = "0.2.3"
 __author__ = "Calimero Ltd."
 __email__ = "engineering@calimero.network"
